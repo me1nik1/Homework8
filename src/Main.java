cfdcec290018b0f06775fa9799241b4713f05842
Регина <reginagatiyatullina@icloud.com>
2024-05-14 23:10:24 +0500

    dz исправлено

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,36 @@ public class Main {
         numbers[0] = 1;
         numbers[1] = 2;
         numbers[2] = 3;
+
+        double[] dnumbers = {1.57, 7.654, 9.986};
+
+        int[] inum = {2, 14, 57, 25};
+
+        System.out.println();
+
+        System.out.println("Задача 2");
+
         System.out.print(numbers[0] + ", ");
         System.out.print(numbers[1] + ", ");
         System.out.print(numbers[2]);
 
         System.out.println();
-        System.out.println();
 
-        System.out.println("Задача 2");
-
-        double[] dnumbers = {1.57, 7.654, 9.986};
         for (int i = 0; i < dnumbers.length; i++) {
             System.out.print(dnumbers[i]);
             if (i < dnumbers.length - 1) {
                 System.out.print(", ");
             }
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < inum.length; i++) {
+            System.out.print(inum[i]);
+            if (i < inum.length - 1) {
+                System.out.print(", ");
+            }
+
         }
         System.out.println();
         System.out.println();
@@ -30,12 +45,30 @@ public class Main {
         System.out.println("Задача 3");
 
         for (int i = numbers.length - 1; i >= 0; i--) {
-            System.out.print(numbers[i] + ", ");
+            System.out.print(numbers[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
         }
+
         System.out.println();
+
         for (int i = dnumbers.length - 1; i >= 0; i--) {
-            System.out.print(dnumbers[i] + ", ");
+            System.out.print(dnumbers[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
         }
+
+        System.out.println();
+
+        for (int i = inum.length - 1; i >= 0; i--) {
+            System.out.print(inum[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+
 
         System.out.println();
         System.out.println();
@@ -50,6 +83,7 @@ public class Main {
             }
             System.out.println(Arrays.toString(numbers));
         }
+
 
     }
 }
